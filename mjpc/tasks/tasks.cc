cae52f6b06dad2365fb04e34910f85f8a4dbc2ca
Ozan Oğuztüzün <81580291+ozan-oguztuzun@users.noreply.github.com>
2026-04-22 23:13:02 -0400

    Update tasks.cc

--- mjpc/tasks/tasks.cc
+++ mjpc/tasks/tasks.cc
@@ -60,7 +60,6 @@ std::vector<std::shared_ptr<Task>> GetTasks() {
       std::make_shared<humanoid::Tracking>(),
       std::make_shared<humanoid::Walk>(),
       std::make_shared<manipulation::Bring>(),
-      std::make_shared<Turtle3DSwimming>(),
       // DEEPMIND INTERNAL TASKS
       std::make_shared<OP3>(),
       std::make_shared<Panda>(),
